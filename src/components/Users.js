d9d1118770c1903c57ab70138784ee231d8b94f0
Lucas André <lucas.cancado89@gmail.com>
2021-05-06 15:44:47 -0300

    finito

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
+// arquivo Users.js
+import React from 'react';
 
-class Users extends Component {
+class Users extends React.Component {
+
   render() {
     return (
-      <div>
-        <h2>Users</h2>
-        <p> My awesome Users component </p>
-      </div>
-    );
+  <div>
+    <h2> Users </h2>
+    <p> { this.props.greetingMessage }, this is my awesome Users component { this.props.match.params.id } </p>
+  </div>
+    )
   }
-};
+}
 
 export default Users;
